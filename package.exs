a953a4e201eb92ac90856a697e34c429fd06aa36
meh <meh@schizofreni.co>
2013-04-03 05:50:13 +0200

    package: update repository

--- package.exs
+++ package.exs
@@ -4,5 +4,5 @@ Expm.Package.new(
   version: "0.0.1",
   keywords: ["decimal", "math"],
   maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
-  repositories: [[github: "meh/elixir-decimal"]]
+  repositories: [[github: "meh/elixir-decimal", tag: "v0.0.1"]]
 )
