4cc66b967bab58e35e9ef081aeecadda86ebba1e
meh <meh@schizofreni.co>
2013-04-09 03:49:37 +0200

    package: add license

--- package.exs
+++ package.exs
@@ -2,6 +2,7 @@ Expm.Package.new(
   name: "decimex",
   description: "Wrapper for erlang-decimal",
   version: "0.0.1",
+  licenses: [[name: "WTFPL"]],
   keywords: ["decimal", "math"],
   maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
   repositories: [[github: "meh/decimex", tag: "v0.0.1"]]
