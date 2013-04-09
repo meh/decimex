d5e79c1d96771f1e48b13d8a0727da6314161f43
meh <meh@schizofreni.co>
2013-04-09 15:07:11 +0200

    Bump version

--- package.exs
+++ package.exs
@@ -1,9 +1,9 @@
 Expm.Package.new(
   name: "decimex",
   description: "Wrapper for erlang-decimal",
-  version: "0.0.1",
+  version: "0.0.2",
   licenses: [[name: "WTFPL"]],
   keywords: ["decimal", "math"],
   maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
-  repositories: [[github: "meh/decimex", tag: "v0.0.1"]]
+  repositories: [[github: "meh/decimex", tag: "v0.0.2"]]
 )
